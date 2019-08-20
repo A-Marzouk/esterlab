b6a48bfc523f585bc6ffc4a104b3b1cad1b00d17
Ahmed-pc <perfect_ahmed@yahoo.com>
2019-08-20 12:58:39 +0300

    red colored buttons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,7 +18,7 @@
               </span>
                         </p>
                         <p><a href="javascript:void(0)" data-toggle="modal" data-target="#contactUsModal"
-                              class="btn btn-white-outline border-w-2 btn-md"> @lang('index/slide.btn_text')</a></p>
+                              class="btn btn-red-outline border-w-2 btn-md"> @lang('index/slide.btn_text')</a></p>
                     </div>
                 </div>
             </div>
@@ -357,7 +357,7 @@
                                 @lang('index/contact.address_title')
                             </div>
                             <div class="col-md-4">
-                                <a href="https://www.google.com/maps/place/ул.+Сечевых+Стрельцов,+60,+Киев,+04050/@50.4565667,30.4894762,17z/data=!4m5!3m4!1s0x40d4ce63673bc087:0xb0a3a2c1274c970!8m2!3d50.4569751!4d30.4899461/">
+                                <a target="_blank" href="https://www.google.com/maps/place/ул.+Сечевых+Стрельцов,+60,+Киев,+04050/@50.4565667,30.4894762,17z/data=!4m5!3m4!1s0x40d4ce63673bc087:0xb0a3a2c1274c970!8m2!3d50.4569751!4d30.4899461/">
                                     @lang('index/contact.address')
                                 </a><br>
                             </div>
