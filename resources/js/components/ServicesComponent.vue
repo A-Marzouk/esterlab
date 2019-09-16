c1a4e2a6e34cbfcfb6dfaa4665b129b0770b3f6b
Ahmed-pc <perfect_ahmed@yahoo.com>
2019-09-16 21:57:12 +0300

    service boxes (Slick slider)

--- resources/js/components/ServicesComponent.vue
+++ resources/js/components/ServicesComponent.vue
@@ -1,114 +1,182 @@
 <template>
-    <div>
-        <div class="solution-box-wrapper justify-content-center">
-            <div class="solution-box">
-                <div class="box-header">
-                   {{ languageData.services }}
-                </div>
-                <div class="solution-title">
-                   {{ languageData.math_modeling }}
-                </div>
-                <div class="solution-sub-title">
-                   {{ languageData.math_modeling_sub_head}}
-                </div>
-                <div class="solution-description">
-                   {{ languageData.math_modeling_explain}}
-                </div>
+    <div class="wrapper">
 
-                <div class="discover-btn">
-                    <a href="/math-modeling">{{languageData.read_more_btn}}</a>
+        <slick ref="slick" :options="slickOptions" class="w-100">
+            <div class="solution-box-wrapper justify-content-center">
+                <div class="solution-box">
+                    <div class="box-header">
+                        {{ languageData.services }}
+                    </div>
+
+                    <div>
+                        <div class="solution-title">
+                            {{ languageData.math_modeling }}
+                        </div>
+                        <div class="solution-sub-title">
+                            {{ languageData.math_modeling_sub_head}}
+                        </div>
+                        <div class="solution-description">
+                            {{ languageData.math_modeling_explain}}
+                        </div>
+
+                        <div class="discover-btn">
+                            <a href="/math-modeling">{{languageData.read_more_btn}}</a>
+                        </div>
+                    </div>
+
                 </div>
             </div>
-        </div>
+            <div class="solution-box-wrapper justify-content-center">
+                <div class="solution-box">
+                    <div class="box-header">
+                        {{ languageData.services }}
+                    </div>
 
-        <div class="solution-box-wrapper justify-content-center">
-            <div class="solution-box">
-                <div class="box-header">
-                    {{ languageData.services }}
-                </div>
-                <div class="solution-title">
-                    {{ languageData.designing }}
-                </div>
-                <div class="solution-sub-title">
-                    {{ languageData.development_mechanisms}}
-                </div>
-                <div class="solution-description" v-html="languageData.designing_explain">
+                    <div>
+                        <div class="solution-title development">
+                            {{ languageData.designing }}
+                        </div>
+                        <div class="solution-sub-title">
+                            {{ languageData.development_mechanisms}}
+                        </div>
+                        <div class="solution-description" v-html="languageData.designing_explain">
 
-                </div>
+                        </div>
 
-                <div class="discover-btn">
-                    <a href="/designing-mechanisms">{{languageData.read_more_btn}}</a>
+                        <div class="discover-btn">
+                            <a href="/designing-mechanisms">{{languageData.read_more_btn}}</a>
+                        </div>
+                    </div>
+
                 </div>
             </div>
-        </div>
+            <div class="solution-box-wrapper justify-content-center">
+                <div class="solution-box">
+                    <div class="box-header">
+                        {{ languageData.services }}
+                    </div>
+                    <div class="solution-title">
+                        {{ languageData.expertise }}
+                    </div>
+                    <div class="solution-description" v-html="languageData.expertise_explain">
 
+                    </div>
 
-        <div class="solution-box-wrapper justify-content-center">
-            <div class="solution-box">
-                <div class="box-header">
-                    {{ languageData.services }}
-                </div>
-                <div class="solution-title">
-                    {{ languageData.expertise }}
-                </div>
-                <div class="solution-description" v-html="languageData.expertise_explain">
-
-                </div>
-
-                <div class="discover-btn">
-                    <a href="/expertise-of-mechanisms">{{languageData.read_more_btn}}</a>
+                    <div class="discover-btn">
+                        <a href="/expertise-of-mechanisms">{{languageData.read_more_btn}}</a>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div class="solution-box-wrapper justify-content-center">
+                <div class="solution-box">
+                    <div class="box-header">
+                        {{ languageData.services }}
+                    </div>
+                    <div class="solution-title development">
+                        {{ languageData.development_technical }}
+                    </div>
+                    <div class="solution-description" v-html="languageData.development_technical_explain">
 
-        <div class="solution-box-wrapper justify-content-center">
-            <div class="solution-box">
-                <div class="box-header">
-                    {{ languageData.services }}
-                </div>
-                <div class="solution-title development">
-                    {{ languageData.development_technical }}
-                </div>
-                <div class="solution-description" v-html="languageData.development_technical_explain">
+                    </div>
 
-                </div>
-
-                <div class="discover-btn">
-                    <a href="/technical-development">{{languageData.read_more_btn}}</a>
+                    <div class="discover-btn">
+                        <a href="/technical-development">{{languageData.read_more_btn}}</a>
+                    </div>
                 </div>
             </div>
-        </div>
-
+        </slick>
 
     </div>
 </template>
 
 <script>
-    import translations from '../lang/en/index'
-    
+    import translations from '../lang/en/index';
+    import Slick from 'vue-slick';
+
     export default {
-        props:['lang'],
+        props: ['lang'],
+        components: {Slick},
         name: "ServicesComponent",
-        data(){
-            return{
-                languageData:{}
+        data() {
+            return {
+                languageData: {},
+                slickOptions: {
+                    slidesToShow: 2,
+                    responsive: [
+                        {
+                            breakpoint: 1024,
+                            settings: {
+                                slidesToShow: 2,
+                                slidesToScroll: 2,
+                                infinite: true,
+                                dots: true
+                            }
+                        },
+                        {
+                            breakpoint: 600,
+                            settings: {
+                                slidesToShow: 1,
+                                slidesToScroll: 1,
+                                arrows:false,
+                                dots: true
+                            }
+                        },
+                        {
+                            breakpoint: 480,
+                            settings: {
+                                slidesToShow: 1,
+                                slidesToScroll: 1,
+                                arrows:false,
+                                dots: true
+                            }
+                        }
+                    ]
+                },
             }
         },
-        methods:{
-
+        methods: {
+            reInit() {
+                // Helpful if you have to deal with v-for to update dynamic lists
+                this.$nextTick(() => {
+                    this.$refs.slick.reSlick();
+                });
+            },
         },
         mounted() {
-            this.languageData = translations[this.lang] ;
+            this.languageData = translations[this.lang];
+            this.reInit();
         }
     }
 </script>
 
 <style lang="scss" scoped>
-.development{
-    font-size: 26px !important;
-    @media (max-width: 600px) {
-        font-size: 20px !important;
-        padding-bottom: 12px;
+    .development {
+        font-size: 26px !important;
+        @media (max-width: 600px) {
+            font-size: 20px !important;
+            padding-bottom: 12px;
+        }
     }
-}
+
+    .wrapper {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+    .solution-box-wrapper {
+        .solution-box {
+            width: 520px !important;
+            height: 515px !important;
+            @media (max-width: 600px) {
+                width: 100% !important;
+                height: 450px !important;
+            }
+            @media (max-width: 400px) {
+                width: 100% !important;
+                height: 530px !important;
+            }
+        }
+    }
+
 </style>
